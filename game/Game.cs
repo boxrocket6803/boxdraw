04570a9857c23f28e22ff5cbba7e9ed65ae08a25
boxrocket <69829964+johncosfm@users.noreply.github.com>
2026-01-03 13:13:29 -0600

    remove game specific stuff

--- game/Game.cs
+++
@@ -1,9 +0,0 @@
-﻿public static class Game {
-	public static Engine Engine {get; private set;} = new();
-
-	public static void Main() {
-		Engine.Init();
-		Scene.Active.Add<GameManager>();
-		Engine.Run();
-	}
-}
